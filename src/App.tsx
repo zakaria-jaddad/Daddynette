468191b8b66e76441b370fbb643af2f80655983d
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2024-09-05 20:23:25 +0100

    Add shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import "./App.css";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 function App() {
-  return <>Hello Daddynette!</>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
+  );
 }
 
 export default App;
